4ff603249f22332ce8e76f1b6863053283179722
Doohee Kang <iro@irodev.xyz>
2023-06-21 15:31:34 +0900

    Remove suppress

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-@file:Suppress("VulnerableLibrariesLocal")
-
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.text.SimpleDateFormat
