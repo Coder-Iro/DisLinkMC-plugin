115fd6373b63c49da9580cbc9049f0fe3e81cfe4
Doohee Kang <iro@irodev.xyz>
2023-06-25 16:07:38 +0900

    Add gradle build scan 2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "dislinkmc"
+
+plugins {
+    id("com.gradle.enterprise") version ("3.13.4")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
