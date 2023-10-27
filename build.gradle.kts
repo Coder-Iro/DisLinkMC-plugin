a2145917a754c113487d801f432b32c7c5c36420
Doohee Kang <iro@irodev.xyz>
2023-10-27 17:56:28 +0900

    Change some dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,6 @@ repositories {
     maven("https://repo.papermc.io/repository/maven-public/")
 }
 
-val exposedVersion: String by project
 dependencies {
     @Suppress("VulnerableLibrariesLocal", "RedundantSuppression")
     compileOnly("com.velocitypowered:velocity-api:3.1.1")
@@ -41,9 +40,9 @@ dependencies {
     implementation("net.dv8tion:JDA:5.0.0-beta.15") {
         exclude(module = "opus-java")
     }
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-core:0.44.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.44.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.44.1")
 }
 
 java {
@@ -58,11 +57,6 @@ tasks {
     withType<ShadowJar> {
         dependencies {
             exclude(dependency("org.slf4j:slf4j-api"))
-        }
-        minimize {
-            exclude(dependency("org.jetbrains.exposed:.*:$exposedVersion"))
-            exclude(dependency("org.mariadb.jdbc:mariadb-java-client:3.2.0"))
-            exclude(dependency("org.apache.logging.log4j:log4j-slf4j-impl"))
         }
     }
     withType<KotlinCompile> {
