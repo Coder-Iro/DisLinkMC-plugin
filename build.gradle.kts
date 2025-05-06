b62651a1be046329d233c26e3467b69b4e50018c
Doohee Kang <iro@irodev.xyz>
2025-05-06 16:00:49 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ repositories {
 
 dependencies {
     @Suppress("VulnerableLibrariesLocal", "RedundantSuppression")
-    compileOnly("com.velocitypowered:velocity-api:3.3.0-SNAPSHOT")
+    compileOnly("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.24.3")
     implementation("org.mariadb.jdbc:mariadb-java-client:3.5.3")
     implementation("net.dv8tion:JDA:5.5.1") {
